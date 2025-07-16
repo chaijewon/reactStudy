8c9b046b8c69e1e651a4f83ad8e35e202d3a31c1
chaijewon <vcandjava@nate.com>
2025-07-16 12:38:52 +0900

    666

--- react-project4/src/App.js
+++ react-project4/src/App.js
@@ -1,7 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState,Fragment } from 'react';
+// 라이브러리 사용 
+/*
+    state : 데이터 변경시마다 다시 브라우저로 변경된 데이터를 전송
+    const [movie,setMovie]=useState({})
+                              ------ 상태 (데이터 변경 감지)
+                                             |
+                                            화면 변경
+    프로그램 단점 => 한번 수행하면 다시 수행 할 수 없다 
 
+    => 1
+       2
+       3 => 1번부터 다시 수행 => let , var 
+       4
+       5
+       => react는 state 프로그램 (데이터 관리 프로그램)
+       LLM /  랭체인 
+*/
 function App(props) {
+  const [movie,setMovie]=useState({})
+  /*
+     정수 useState(0)
+     문자 useState('')
+     배열 useState([])
+  */
+  const detail=(m)=>{
+    //movie=m
+    setMovie(m) // 값을 저장 movie. => 처음부터 다시 실행
+  }
   return (
     <div className='container'>
       <div className='col-sm-8'>
@@ -18,7 +45,7 @@ function App(props) {
           <tbody>
             {
               props.movie.map(m=>
-                 <tr className='dataTr'>
+                 <tr className='dataTr' onMouseOver={()=>detail(m)}>
                   <td className='text-center'>{m.rank}</td>
                   <td className='text-center'>
                     <img src={"https://www.kobis.or.kr/"+m.thumbUrl} 
@@ -34,10 +61,45 @@ function App(props) {
         </table>
       </div>
       <div className='col-sm-4'>
-
+        <MovieDetail movie={movie}/>
       </div>
     </div>
   );
+}
+function MovieDetail(props){
+  return (
+    <Fragment>
+     <h1 className='text-center'>영화 상세</h1>
+     <table className='table'>
+      <tbody>
+        <tr>
+          <td width={"30%"} className='text-center' rowSpan={"5"}>
+            <img src={"https://www.kobis.or.kr/"+props.movie.thumbUrl} style={{"width":"100%"}}/>
+          </td>
+          <td colSpan={"2"}>
+            <h3>{props.movie.movieNm}</h3>
+          </td>
+        </tr>
+        <tr>
+          <th width="15%">감독</th>
+          <td width="55%">{props.movie.director}</td>
+        </tr>
+        <tr>
+          <th width="15%">상영일</th>
+          <td width="55%">{props.movie.startDate}</td>
+        </tr>
+        <tr>
+          <th width="15%">장르</th>
+          <td width="55%">{props.movie.genre}</td>
+        </tr>
+        <tr>
+          <th width="15%">등급</th>
+          <td width="55%">{props.movie.watchGradeNm}</td>
+        </tr>
+      </tbody>
+     </table>
+    </Fragment>
+  )
 }
 
 export default App;
